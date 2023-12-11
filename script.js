1716ee08d5c1cbce2d2a43c24b59fb10afba7e65
anas20023 <yorder07@gmail.com>
2023-12-11 22:26:57 +0600

    Fix navigation bar functionality

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ function CV() {
 function Close_alt() {
   document.getElementById("al_open").style.display = "none";
 }
+
+
+// For Navigation Bar
+let hide=document.querySelector("#covered");
+let cross=document.querySelector("#Showme");
+
+function Hide_btn()
+{
+  hide.classList.remove("-left-full");
+  hide.classList.add("left-0");
+
+}
+function Showme(){
+  hide.classList.remove("left-0");
+  hide.classList.add("-left-full");
+}
