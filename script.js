b4295e6de7b7bf27188294ce04357611a24b2b57
anas20023 <yorder07@gmail.com>
2023-12-11 23:20:26 +0600

    Fix linting issues and update navigation bar

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
+// For CV
+let alt_cv;
+alt_cv = document.querySelector("#not_av");
+
 function CV() {
-  document.getElementById("al_open").style.display = "flex";
+  alt_cv.classList.remove("hidden");
+  alt_cv.classList.add("flex");
 }
 function Close_alt() {
-  document.getElementById("al_open").style.display = "none";
+  alt_cv.classList.remove("flex");
+  alt_cv.classList.add("hidden");
 }
-
 
 // For Navigation Bar
-let hide=document.querySelector("#covered");
-let cross=document.querySelector("#Showme");
+let hide = document.querySelector("#covered");
+let cross = document.querySelector("#Showme");
 
-function Hide_btn()
-{
+function Hide_btn() {
   hide.classList.remove("-left-full");
   hide.classList.add("left-0");
-
 }
-function Showme(){
+function Showme() {
   hide.classList.remove("left-0");
   hide.classList.add("-left-full");
 }
